cf27d325e50ba64447d7291ee7b3db300bc921e6
mkochalko <matthew.kochalko@gmail.com>
2020-02-06 14:37:48 -0800

    Refactor D3 graph

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -15,15 +15,15 @@ class Footer {
         <div class="footer-container">
             <div class="footer-section-container">
                 <h4>Contact me here</h4>
-                <a href="https://www.linkedin.com/in/mkochalko/"><img src="/javascripts/images/LinkedIn_logo_initials.png" alt="LinkedIn"></a>
+                <a href="https://www.linkedin.com/in/mkochalko/" target="_blank"><img src="/javascripts/images/LinkedIn_logo_initials.png" alt="LinkedIn"></a>
             </div>
             <div class="footer-section-container">
                 <h4>Checkout out my GitHub repo</h4>
-                <a href="https://github.com/mkochalko/6degreesofbacon"><img src="/javascripts/images/github.png" alt="GitHub"></a>
+                <a href="https://github.com/mkochalko/6degreesofbacon" target="_blank"><img src="/javascripts/images/github.png" alt="GitHub"></a>
             </div>
             <div class="footer-section-container">
                 <h4>Angel List Profile</h4>
-                <a href="https://angel.co/matthew-kochalko"><img src="/javascripts/images/angellist.png" alt="AngleList"></a>
+                <a href="https://angel.co/matthew-kochalko" target="_blank"><img src="/javascripts/images/angellist.png" alt="AngleList"></a>
             </div>
         </div>
         `
