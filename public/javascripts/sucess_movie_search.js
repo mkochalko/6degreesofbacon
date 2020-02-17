8e2d739f890b9d11fa24d57dfd2ca7ce38cf91dc
mkochalko <matthew.kochalko@gmail.com>
2020-02-16 16:40:33 -0800

    Finish styling

--- public/javascripts/sucess_movie_search.js
+++ public/javascripts/sucess_movie_search.js
@@ -10,11 +10,17 @@ class SuccessMovieSearch {
         // console.log(this.response)
         this.container.innerHTML += `
             <div class="movie-info-container hidden" id="${this.response.Title}">
-                <h3 id="1" data-value="${this.response.Title}" class="movie-info-item">Rated: ${this.response.Rated}</h3>
-                <h3 id="2" data-value="${this.response.Title}" class="movie-info-item">Released: ${this.response.Released}</h3>
-                <h3 id="3" data-value="${this.response.Title}" class="movie-info-item">Runtime: ${this.response.Runtime}</h3>
-                <h3 id="4" data-value="${this.response.Title}" class="movie-info-item">IMDB Rating: ${this.response.imdbRating}</h3>
-                <h3 id="5" data-value="${this.response.Title}" class="movie-info-item">B.O. Earnings: ${this.response.BoxOffice}</h3>
+                <h2>${this.response.Title}</h2>
+                <div class="movie-info-with-poster">
+                    <img src=${this.response.Poster} />
+                    <div class="movie-info-section">
+                        <h3 id="1" data-value="${this.response.Title}" class="movie-info-item">Rated: ${this.response.Rated}</h3>
+                        <h3 id="2" data-value="${this.response.Title}" class="movie-info-item">Released: ${this.response.Released}</h3>
+                        <h3 id="3" data-value="${this.response.Title}" class="movie-info-item">Runtime: ${this.response.Runtime}</h3>
+                        <h3 id="4" data-value="${this.response.Title}" class="movie-info-item">IMDB Rating: ${this.response.imdbRating}</h3>
+                        <h3 id="5" data-value="${this.response.Title}" class="movie-info-item">B.O. Earnings: ${this.response.BoxOffice}</h3>
+                    </div>
+                </div>
             </div>
         `
     }
